d610112259032770086e4ac9dd190de3fdda2418
AanuTT <114654754+Aanu-TT@users.noreply.github.com>
2023-04-08 01:16:27 +0100

    initial commit

--- components/WhoWeAre.jsx
+++ components/WhoWeAre.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function WhoWeAre() {
-  return <section className=""></section>;
+  return <section className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus optio laboriosam neque odio adipisci explicabo eaque praesentium incidunt non quaerat quibusdam, sunt odit eius veniam nisi. Fuga magni animi itaque!</section>;
 }
